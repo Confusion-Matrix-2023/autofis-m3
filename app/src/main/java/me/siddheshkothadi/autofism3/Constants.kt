bc3816d5d7a4e67e4660130cd9f1ddad78a82e40
Siddhesh Kothadi <realsiddheshko@gmail.com>
2022-08-24 14:19:50 +0530

    New fishnet yolo5m model

--- app/src/main/java/me/siddheshkothadi/autofism3/Constants.kt
+++ app/src/main/java/me/siddheshkothadi/autofism3/Constants.kt
@@ -3,8 +3,8 @@ package me.siddheshkothadi.autofism3
 import me.siddheshkothadi.autofism3.ui.Language
 
 object Constants {
-    const val MINIMUM_CONFIDENCE_TF_OD_API = 0.9f
-    const val MODEL_FILE_PATH = "yolov5s_FinalData.tflite"
+    const val MINIMUM_CONFIDENCE_TF_OD_API = 0.6f
+    const val MODEL_FILE_PATH = "yolov5m_Fishnet.tflite"
     const val LANGUAGE_KEY = "language_key"
     const val DEFAULT_LOCALE = "en"
     const val JWT_SECRET = "f00t4f30321@@0439!2#@am"
